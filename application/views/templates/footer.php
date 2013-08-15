c2fa9567f98b12c48f72a41d5a41dedd88d2b729
José Hurtado <mymail@mail.com>
2013-08-15 03:43:34 -0400

    Versión beta
    solo navbar

---
+++ application/views/templates/footer.php
@@ -0,0 +1,10 @@
+</div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?= base_url() .APPPATH ?>js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script src="<?= base_url() .APPPATH?>js/bootstrap.min.js"></script>
+
+        <script src="<?= base_url() .APPPATH?>js/main.js"></script>
+    </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="<?= base_url() .APPPATH ?>css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 70px;
+                padding-bottom: 40px;
+            }
+        </style>
+
+        <script src="<?= base_url() .APPPATH?>js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
